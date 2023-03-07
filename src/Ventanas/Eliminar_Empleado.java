849717873a14a5dd3f741733bcbe0786db52e458
FreddyGomezO <freddy.gomez.est@tecazuay.edu.ec>
2023-03-06 23:04:41 -0500

    JFrame Empleado

--- src/Ventanas/Eliminar_Empleado.java
+++ src/Ventanas/Eliminar_Empleado.java
@@ -9,12 +9,12 @@ package Ventanas;
  *
  * @author Lenovo
  */
-public class Empleado extends javax.swing.JFrame {
+public class Eliminar_Empleado extends javax.swing.JFrame {
 
     /**
-     * Creates new form Empleado
+     * Creates new form Eliminar_Empleado
      */
-    public Empleado() {
+    public Eliminar_Empleado() {
         initComponents();
     }
 
@@ -60,20 +60,20 @@ public class Empleado extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Empleado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Eliminar_Empleado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Empleado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Eliminar_Empleado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Empleado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Eliminar_Empleado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Empleado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Eliminar_Empleado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Empleado().setVisible(true);
+                new Eliminar_Empleado().setVisible(true);
             }
         });
     }
